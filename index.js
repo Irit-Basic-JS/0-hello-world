c459685bb425ee5ad49393c4c1b49f5965856e26
AdIVIuH <kvashnin.2001@yandex.ru>
2021-06-04 12:54:07 +0500

    Комит

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function sumArr (arr) {
-    let sum = arr => arr.reduce((res, el) => res + (Array.isArray(el) ? sum(el) : el), 0);
-    return sum(arr);
-}
-
-let arr = [1, [2, [3, [4]]]];
-
-console.log(sumArr(arr));
+console.log("Hello, Dmitry");
