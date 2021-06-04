1b3a04e2a76364a1b43ba434a9be23ae8e1fb4b2
AdIVIuH <kvashnin.2001@yandex.ru>
2021-06-04 12:48:01 +0500

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+function sumArr (arr) {
+    let sum = arr => arr.reduce((res, el) => res + (Array.isArray(el) ? sum(el) : el), 0);
+    return sum(arr);
+}
+
+let arr = [1, [2, [3, [4]]]];
+
+console.log(sumArr(arr));
